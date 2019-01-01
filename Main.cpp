95b7b194a872bb6d65f8cb01d94a8b23d0fcf4f9
Pawel <CiemnyPawel>
2019-01-01 23:33:37 +0100

    Add producer and part of main

--- Main.cpp
+++ Main.cpp
@@ -3,35 +3,35 @@
 #include <queue>
 #include <pthread.h>
 
+#define numberOfBuffers 20 // can't use global variable
+size_t buffersLength = 10;
+size_t numberOfProducedMessages = 20;
+size_t numberOfProducers = 10;
+
 class buffer : public Monitor
 {
 public:
-buffer(char Id, unsigned int bufferLength) : id(Id), bufferLength(bufferLength)
-{
+buffer(){
 }
-
 unsigned int checkBufferSize()
 {
         enter();
         return (unsigned int) bufferQueue.size();
 }
-
 void unlockBufferAfterChoosing()
 {
         leave();
 }
-
 void putMessage(unsigned int message)
 {
         enter();
-        if(bufferQueue.size() == bufferLength)
+        if(bufferQueue.size() == buffersLength)
                 wait(notFull);
         bufferQueue.push(message);
         if(bufferQueue.size() == 1)
                 signal(notEmpty);
         leave();
 }
-
 unsigned int getMessage()
 {
         enter();
@@ -39,18 +39,76 @@ unsigned int getMessage()
                 wait(notEmpty);
         unsigned int message = bufferQueue.front();
         bufferQueue.pop();
-        if(bufferQueue.size() == bufferLength - 1)
+        if(bufferQueue.size() == buffersLength - 1)
         {
                 signal(notFull);
         }
         leave();
         return message;
 }
-
 private:
 Condition notFull;
 Condition notEmpty;
 std::queue<unsigned int> bufferQueue;
-const char id;
-const unsigned int bufferLength;
 };
+
+buffer arrayOfBuffers[numberOfBuffers];
+
+unsigned int IndepRand()
+{
+        FILE * F = fopen("/dev/urandom", "r");
+        if(!F)
+        {
+                printf("Cannot open urandom...\n");
+                abort();
+        }
+        unsigned int randomValue;
+        fread((char *) &randomValue, 1, sizeof(unsigned int), F);
+        fclose(F);
+        return randomValue;
+}
+
+void consumer()
+{
+        size_t numberOfAllMessages = numberOfProducedMessages * numberOfProducers;
+        for(auto it = 0; it < numberOfAllMessages; it++)
+        {
+
+        }
+}
+
+void producer(size_t producerNr)
+{
+        for(auto it = 0; it < numberOfProducedMessages; it++)
+        {
+                usleep(IndepRand() % 100000);
+                unsigned int message = producerNr * 1000 + IndepRand() % 100;
+                unsigned int temp, nrMinBuffer, minValue;
+                minValue = buffersLength +1;
+                for(auto i = 0; i < numberOfBuffers; i++)
+                {
+                        temp = arrayOfBuffers[i].checkBufferSize();
+                        if(temp < minValue)
+                        {
+                                minValue = temp;
+                                nrMinBuffer = i;
+                        }
+                }
+                arrayOfBuffers[nrMinBuffer].putMessage(message);
+                for(auto i = 0; i < numberOfBuffers; i++)
+                        arrayOfBuffers[i].unlockBufferAfterChoosing();
+        }
+}
+int main(int ArgC, char ** ArgV)
+{
+        if(ArgC != 4)
+        {
+                printf("%s =: Liczba producentow || Liczba wiadomosci od kazdego producenta || Dlugosc buforow\n", ArgV[0]);
+                return 1;
+        }
+        numberOfProducers = (size_t) atoi(ArgV[1]);
+        numberOfProducedMessages = (size_t) atoi(ArgV[2]);
+        buffersLength = (size_t) atoi(ArgV[3]);
+
+        return 0;
+}
